a8adc3fcd483ab2edd5390314c8e7713d702c3ca
Mikka <mikka@sendke.eu>
2023-10-08 19:33:16 +0200

    Removed console.log()

--- index.js
+++ index.js
@@ -48,7 +48,6 @@ v1TwitchRouter.get('/messages/:channel_name', (req, res) => {
 
 v1TwitchRouter.post('/insertMessage', async (req, res) => {
     try {
-        console.log(req.body);
         if (!req.body.timestamp || !req.body.channel || !req.body.user || !req.body.content || !req.body.display_name) {
             console.log('Missing required parameters');
             res.status(400).send('Missing required parameters');
