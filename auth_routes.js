54b854b1791267b70e3e904d3e7889df024974ca
Mikka <mikka@sendke.eu>
2023-11-18 14:22:56 +0100

    Formatting

--- auth_routes.js
+++ auth_routes.js
@@ -1,14 +1,14 @@
 import express from "express";
-import { v1Router } from './index.js';
+import { v1Router } from "./index.js";
 
 const authRouter = express.Router();
 
-v1Router.use('/auth', authRouter);
+v1Router.use("/auth", authRouter);
 
-authRouter.post('/login', (req, res) => {
-    res.send('login');
+authRouter.post("/login", (req, res) => {
+    res.send("login");
 });
 
 authRouter.post("/register", (req, res) => {
-    res.send('register');
-})
+    res.send("register");
+});
