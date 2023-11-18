cb5019510fd6a3a49d8cad8dd36dffe395907a64
Mikka <mikka@sendke.eu>
2023-11-18 15:18:20 +0100

    A small refactor bigger one later

--- auth.js
+++ auth.js
@@ -18,35 +18,29 @@ import { pool } from "./db.js";
  */
 export async function getUserFromRequest(req) {
     if (!req || !req.headers || !req.headers.authorization) return null;
+
     const authHeader = req.headers.authorization;
     if (!authHeader) return null;
 
     const splitHeader = authHeader.trim().split(" ");
-
     if (splitHeader.length !== 2 || splitHeader[0].toLowerCase() !== "bearer") {
         return null;
     }
-    const token = splitHeader[1];
 
+    const token = splitHeader[1];
     const hashedToken = createHash("sha256").update(token).digest("base64");
 
-    let rows;
     try {
-        rows = await pool.query(
+        const result = await pool.query(
             "SELECT users.* FROM users JOIN auth_tokens ON auth_tokens.user_id = users.id LIMIT 1",
             [hashedToken],
         );
+        if (result.rowCount !== 1) return null;
+        return result.rows[0];
     } catch (e) {
         console.error(e);
         return null;
     }
-
-    if (rows.rowCount !== 1) return null;
-    const user = rows.rows[0];
-
-    if (!user) return null;
-
-    return user;
 }
 
 /**
@@ -58,15 +52,14 @@ export async function getUserFromRequest(req) {
 export async function login(username, password) {
     if (!username || !password) return null;
 
-    const user = getUserByUsername(username);
-    if (!user) return null;
+    const user = await getUserByUsername(username);
 
-    if (bcrypt.compareSync(password, user.password_hash)) {
-        const token = createAuthToken(user.id);
-        if (!token) return null;
-        return token;
+    if (!user || !bcrypt.compareSync(password, user.password_hash)) {
+        return null;
     }
-    return null;
+    const token = await createAuthToken(user.id);
+    if (!token) return null;
+    return token;
 }
 
 /**
@@ -77,13 +70,13 @@ export async function login(username, password) {
  */
 export async function register(username, password) {
     if (!username || !password) return null;
+
     try {
-        if (
-            pool.query("SELECT * FROM users WHERE username = $1 LIMIT 1", [
-                username,
-            ]).rowCount === 1
-        )
-            return null;
+        const existingUser = await pool.query(
+            "SELECT * FROM users WHERE username = $1 LIMIT 1",
+            [username],
+        );
+        if (existingUser.rowCount !== 0) return null;
     } catch (e) {
         console.error(e);
         return null;
@@ -93,7 +86,7 @@ export async function register(username, password) {
     const hashedPassword = bcrypt.hashSync(password, salt);
 
     try {
-        pool.query(
+        await pool.query(
             "INSERT INTO users (username, password_hash) VALUES ($1, $2) RETURNING id",
             [username, hashedPassword],
         );
@@ -122,11 +115,14 @@ async function createAuthToken(userId) {
             "SELECT * FROM users WHERE id = $1 LIMIT 1",
             [userId],
         );
+
         if (result.rows.rowCount !== 1) return null;
+
         await pool.query(
             "INSERT INTO auth_tokens (user_id, token) VALUES ($1, $2)",
             [userId, hashedToken],
         );
+        return token;
     } catch (e) {
         console.error(e);
         return null;
@@ -140,17 +136,18 @@ async function createAuthToken(userId) {
  */
 async function getUserById(id) {
     if (!id) return null;
-    let rows;
+
     try {
-        rows = await pool.query("SELECT * FROM users WHERE id = $1 LIMIT 1", [
-            id,
-        ]);
+        const result = await pool.query(
+            "SELECT * FROM users WHERE id = $1 LIMIT 1",
+            [id],
+        );
+        if (result.rowCount !== 1) return null;
+        return result.rows[0];
     } catch (e) {
         console.error(e);
         return null;
     }
-    if (rows.rowCount !== 1) return null;
-    return rows.rows[0];
 }
 
 /**
@@ -160,16 +157,17 @@ async function getUserById(id) {
  */
 async function getUserByUsername(username) {
     if (!username) return null;
-    let rows;
+
     try {
-        rows = await pool.query(
+        const result = await pool.query(
             "SELECT * FROM users WHERE username = $1 LIMIT 1",
             [username],
         );
+
+        if (result.rowCount !== 1) return null;
+        return result.rows[0];
     } catch (e) {
         console.error(e);
         return null;
     }
-    if (rows.rowCount !== 1) return null;
-    return rows.rows[0];
 }
--- auth_routes.js
+++ auth_routes.js
@@ -1,13 +1,33 @@
 import express from "express";
+import { login, register } from "./auth.js";
 
 const authRouter = express.Router();
 
-authRouter.post("/login", (req, res) => {
-    res.send("login");
+authRouter.post("/login", async (req, res) => {
+    if (!req.body || !req.body.username || !req.body.password) {
+        res.status(400).send("Bad Request");
+        return;
+    }
+    const token = await login(req.body.username, req.body.password);
+    if (!token) {
+        res.status(401).send("Unauthorized");
+        return;
+    }
+
+    res.send(token);
 });
 
 authRouter.post("/register", (req, res) => {
-    res.send("register");
+    if (!req.body || !req.body.username || !req.body.password) {
+        res.status(400).send("Bad Request");
+        return;
+    }
+    const token = register(req.body.username, req.body.password);
+    if (!token) {
+        res.status(401).send("Unauthorized");
+        return;
+    }
+    res.send(token);
 });
 
 export default authRouter;
